19d03412d4ce1c32a37774d6fc7069c5f1e9e8c2
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-29 18:08:25 -0500

    add/remove comment(s)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-import * as THREE from "three";
+import * as THREE from "./node_modules/three/build/three.module.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000);
+// const scene = new Scene();
+const camera = new THREE.PerspectiveCamera(45, 1920 / 1080, 1, 1000);
+// const camera = new PerspectiveCamera(45, width / height, 1, 1000);
+scene.add(camera);
 
 const init = () => {
   const container = document.querySelector("#container");
   console.log("initialized");
+  console.log(container);
   console.log(container);
   // scene.add(camera);
 };
