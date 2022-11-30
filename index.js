1353234b944b0366d417e04a3cf205a18c7e9130
ch2isk4kos <ch2isk4kos@gmail.com>
2022-11-29 20:21:04 -0500

    add camera, scene/lighting, and mesh

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
 
+// a Camera is your viewpoint of the virtual world
+// new THREE.PerspectiveCamera( perspective view, aspect ratio, near, far );
+const camera = new THREE.PerspectiveCamera(
+  35,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  3000
+);
+
+// a Scene is the container of the virtual world
 const scene = new THREE.Scene();
-// const camera = new PerspectiveCamera(45, width / height, 1, 1000);
-const camera = new THREE.PerspectiveCamera(45, 1920 / 1080, 1, 1000);
+
+const sceneLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(sceneLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+scene.add(pointLight);
+
+// a Mesh is an object in the virtual world
+// const geometry = new THREE.SphereGeometry(15, 32, 16);
+const geometry = new THREE.BoxGeometry(100, 100, 100);
+// const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+const material = new THREE.MeshLambertMaterial({ color: 0xffffff });
+const mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, -1000);
+scene.add(mesh);
+
+// a Renderer displays the virtual world in the browser
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#renderer"),
+  antialias: true,
+});
+
+renderer.setClearColor(0x00ff00);
+// renderer.setPixelRatio(window.devicePixelRatio);
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+const render = () => {
+  mesh.rotation.x += 0.1;
+  mesh.rotation.y += 0.1;
+  renderer.render(scene, camera);
+  requestAnimationFrame(render);
+};
+
+requestAnimationFrame(render);
 
 const init = () => {
-  const container = document.querySelector("#container");
-  console.log("initialized");
-  scene.add(camera);
-  console.log({ scene });
+  console.log("index.js initialized");
+  renderer.render(scene, camera);
 };
 
 init();
